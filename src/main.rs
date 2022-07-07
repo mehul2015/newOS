011b67c8e52cff3d3815176f1da7b1ac5dde6803
mehul2015 <mehuldev2015@gmail.com>
2022-07-07 11:54:02 +0530

    exclude stdlib

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+//main.rs
+
+![no_std]
+//does not include the standara library, because we have to create an exectuable binary that is
+//os independent
+
+
+fn main() {
+    println!("Hello, world!");
+}
